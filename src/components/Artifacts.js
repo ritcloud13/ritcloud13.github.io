e5fb3da71170bc093e314c9dd33baa218dd9b961
Annie Tiet <anniedtiet@gmail.com>
2023-09-25 22:52:03 -0400

    update synopsis and artifacts page

--- src/components/Artifacts.js
+++ src/components/Artifacts.js
@@ -1,10 +1,31 @@
+import Divider from '@mui/material/Divider';
+import Stack from '@mui/material/Stack';
 import Typography from '@mui/material/Typography';
 
 function Artifacts() {
     return (
-        <Typography variant="h4" gutterBottom>
-            Artifacts to be updated.
+
+        <Stack
+            alignItems="center" spacing={3}>
+                <Typography variant="body1" gutterBottom display="hidden">
+            Each team member is responsible for estimating and reporting time/effort worked on project activities. 
+            The time/effort worked is aggregated for the entire team and also reported. 
+            Project artifacts, such as development methodology, product/process metrics, and domain model are displayed on the website for visibility. 
         </Typography>
+            <Typography variant="h4" align="center" gutterBottom>
+                Team Time Tracking
+            </Typography>
+            <iframe title="timeTracking" width="100%" height="500"
+                src="https://docs.google.com/spreadsheets/d/e/2PACX-1vQUX9bb6CrPwsL5qSIGy7Nz0drSNF77nWHiPysF3KN-R1ueufCT9SF3xPCI-kmSOvhQqWcooi_Pi3_j/pubhtml?widget=true&amp;headers=false" />
+            <Divider />
+            <Typography variant="h4" align="center" gutterBottom>
+                4-Ups
+            </Typography>
+            <iframe title="4ups" width="100%" height="500"
+                src="https://docs.google.com/document/d/e/2PACX-1vR5JUTYHJtBAyEXA_rxJ37jOxgBTUyS-AGHF3BYd-JvwKCLR41XPTrdvclGCaWtsK8RBsswG-z7cbj0/pub?embedded=true"/> 
+            <Typography variant="h4" gutterBottom>
+            </Typography>     
+        </Stack>
     );
 }
 
--- src/components/Main.js
+++ src/components/Main.js
@@ -20,12 +20,12 @@ function Main() {
         Project Synopsis
       </Typography>
       <Typography variant="body1" gutterBottom>
-        Developed for Lockheed Martin, this responsive web application used to manage and showcase
-        the workflow of maintenance requests for products, typically software licenses. Owners of
+        Developed for Lockheed Martin, this responsive web application is used to manage and showcase
+        the workflow of maintenance requests for products, typically IT licenses. Owners of
         maintenance requests will be able to import requests from existing CSV Files or manually
         input the data. Relevant users will be notified of upcoming maintenance requests via email.
         The notification cadence is configurable to the organization’s needs, but a default frequency
-        is provided to each request. These users may be able to track and approve payments. Reports
+        is provided to each request. These users may be able to track and approve budgets. Reports
         can be generated on budgets, actual amount paid, and outstanding costs that owners and/or
         owning managers are responsible for. These reports potentially aid in cost analysis and
         budgeting for the department. The project is created leveraging AWS services into a
