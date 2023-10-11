2cefc58631c5e9abd6048da9f06260a581491496
Annie Tiet <anniedtiet@gmail.com>
2023-10-11 15:40:57 -0400

    add additional artifacts

--- src/components/Artifacts.js
+++ src/components/Artifacts.js
@@ -10,21 +10,39 @@ function Artifacts() {
                 <Typography variant="body1" gutterBottom display="hidden">
             Each team member is responsible for estimating and reporting time/effort worked on project activities. 
             The time/effort worked is aggregated for the entire team and also reported. 
-            Project artifacts, such as development methodology, product/process metrics, and domain model are displayed on the website for visibility. 
+            Project artifacts, such as weekly 4 ups, domain model, methodology and metrics, and final project plan are displayed on the website for visibility. 
         </Typography>
             <Typography variant="h4" align="center" gutterBottom>
                 Team Time Tracking
             </Typography>
             <iframe title="timeTracking" width="100%" height="500"
-                src="https://docs.google.com/spreadsheets/d/e/2PACX-1vQUX9bb6CrPwsL5qSIGy7Nz0drSNF77nWHiPysF3KN-R1ueufCT9SF3xPCI-kmSOvhQqWcooi_Pi3_j/pubhtml?widget=true&amp;headers=false" />
+                src="https://docs.google.com/spreadsheets/d/e/2PACX-1vSk5hVQjOallX4PqnrDeqTHdPJrknarVFaakokTYEBS8BU6c78OHhdWDhMlJAYeWg5HH5QwpdXm-MLu/pubhtml?widget=true&amp;headers=false"/>
             <Divider />
             <Typography variant="h4" align="center" gutterBottom>
-                4-Ups
+                4 Ups
             </Typography>
             <iframe title="4ups" width="100%" height="500"
                 src="https://docs.google.com/document/d/e/2PACX-1vR5JUTYHJtBAyEXA_rxJ37jOxgBTUyS-AGHF3BYd-JvwKCLR41XPTrdvclGCaWtsK8RBsswG-z7cbj0/pub?embedded=true"/> 
-            <Typography variant="h4" gutterBottom>
-            </Typography>     
+            <Divider />
+            <Typography variant="h4" align="center" gutterBottom>
+                Domain Model
+            </Typography>
+            <iframe title="domainModel" width="100%" height="500"
+                src="https://viewer.diagrams.net/?highlight=0000FF&nav=1#G1vm9o0tgjrCtWAT8DncvX7knmjUxSO7jP"/>
+            <Divider />
+            <Typography variant="h4" align="center" gutterBottom>
+                Methodology and Metrics
+            </Typography>
+            <iframe title="methodologyMetrics" width="100%" height="500"
+                src="/ProjectMethodology.pdf"/>
+            <Divider />
+            <Typography variant="h4" align="center" gutterBottom>
+                Final Project Plan
+            </Typography>
+            <iframe title="methodologyMetrics" width="100%" height="500"
+                src="/FinalProjectPlan.pdf"/> 
+            <Typography variant="h4" align="center" gutterBottom>
+            </Typography> 
         </Stack>
     );
 }
